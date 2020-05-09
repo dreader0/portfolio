cde4dae07f33693b9403fa43b72278660ff1fde8
Dan <danreader0@gmail.com>
2020-05-09 00:30:51 -0400

    Updates

---
+++ precache-manifest.297edc92d122b713621b1743c3682706.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f63e2a1fd91049c512f47a5200ec5b4",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "cb937a78f71585da3df2",
+    "url": "/portfolio/static/css/2.02b9615e.chunk.css"
+  },
+  {
+    "revision": "19fa2c51d8dbfe9071da",
+    "url": "/portfolio/static/css/main.da8e2f6a.chunk.css"
+  },
+  {
+    "revision": "cb937a78f71585da3df2",
+    "url": "/portfolio/static/js/2.0dcf99f7.chunk.js"
+  },
+  {
+    "revision": "216e537bacc64f9dbaabd26649a9ab95",
+    "url": "/portfolio/static/js/2.0dcf99f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "19fa2c51d8dbfe9071da",
+    "url": "/portfolio/static/js/main.0784c7d6.chunk.js"
+  },
+  {
+    "revision": "e5284505c87d0b24b753",
+    "url": "/portfolio/static/js/runtime-main.f4f2abbf.js"
+  },
+  {
+    "revision": "408ab118f1f5fa603771be386940e5b9",
+    "url": "/portfolio/static/media/DR_3.408ab118.svg"
+  },
+  {
+    "revision": "2dff39ee3981f72bd095444edfd1be97",
+    "url": "/portfolio/static/media/Headshot.2dff39ee.jpg"
+  },
+  {
+    "revision": "a54e129fa7e147d01a415700a909ac1f",
+    "url": "/portfolio/static/media/Resume.a54e129f.pdf"
+  },
+  {
+    "revision": "10c5b73f9a3c09980a4383b55d031ff5",
+    "url": "/portfolio/static/media/smartbrian.10c5b73f.png"
+  }
+]);
